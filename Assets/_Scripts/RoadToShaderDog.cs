d6c2e56335b1c0e449b12e38ede2ccf565b6d719
yankun <yankun@rd.netease.com>
2021-10-25 21:32:51 +0800

    - Add new models.

---
+++ Assets/_Scripts/RoadToShaderDog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadToShaderDog : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("RoadToShaderDog Start()");
+    }
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("RoadToShaderDog OnDestroy()");
+    }
+}
